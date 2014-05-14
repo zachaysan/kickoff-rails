0f5d15e1c10c9768991c016b54141eaf7264892b
Zach Aysan <zachaysan@gmail.com>
2014-05-14 16:08:33 -0400

    Add owned emails relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :owned_emails, foreign_key: :owner_id, class_name: "Email"
+
   # If password_confirmation is passed, business as usual.
   # If not, don't run the validations
   def should_confirm_password?
