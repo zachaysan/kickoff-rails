ee4c57bc29f8a866e14ea1526317b723cf244759
Zach Aysan <zachaysan@gmail.com>
2014-05-15 23:22:26 -0400

    Create access token model.

---
+++ app/models/access_token.rb
@@ -0,0 +1,13 @@
+class AccessToken < ActiveRecord::Base
+
+  belongs_to :user
+
+  validates_presence_of :token
+
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.hex
+  end
+
+end
