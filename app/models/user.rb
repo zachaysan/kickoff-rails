94c78d8e9e15d3f4fd79365152b10db850381ab9
Zach Aysan <zachaysan@gmail.com>
2014-05-20 13:36:50 -0400

    Allow a user to have multiple access tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :owned_emails, foreign_key: :owner_id, class_name: "Email"
-  has_one :access_token
+  has_many :access_tokens
 
   validates_presence_of :username
 
