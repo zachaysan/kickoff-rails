abe12a1b3c00a4b3316f4cd28797562dee14195f
Zach Aysan <zachaysan@gmail.com>
2014-05-21 14:30:31 -0400

    Add some user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,23 @@ describe User do
   let(:user) { FactoryGirl.create(:user) }
 
   it "has a valid factory" do
-    expect(user).to be_valid
+    expect( user ).to be_valid
   end
 
-  it "is invalid without a username" do
+  it "is invalid without a username or email" do
     user.username = nil
+    user.owned_emails.destroy_all
     expect( user ).to_not be_valid
+  end
+
+  it "is valid without email, but with a username" do
+    user.owned_emails.destroy_all
+    expect( user ).to be_valid
+  end
+
+  it "is valid without a username, but with a email" do
+    user.username = nil
+    expect( user ).to be_valid
   end
 
   it "shows a list of owned emails" do
