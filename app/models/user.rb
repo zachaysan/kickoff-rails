dd182cb8b8573c04433cbc038d262d4165cdcc44
Zach Aysan <zachaysan@gmail.com>
2014-05-21 14:28:08 -0400

    Make sure they have either the email or the username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,21 @@ class User < ActiveRecord::Base
   has_many :owned_emails, foreign_key: :owner_id, class_name: "Email"
   has_many :access_tokens
 
-  validates_presence_of :username
+  validates :username, allow_blank: true, uniqueness: {case_sensitive: false}
+  validate :has_email_or_username
 
   # If password_confirmation is passed, business as usual.
   # If not, don't run the validations
   def should_confirm_password?
     password_confirmation.present?
+  end
+
+  def has_email_or_username
+    if !(username or owned_emails.any?)
+      errors.add(:username, "required if no emails")
+    else
+      true
+    end
   end
 
 end
