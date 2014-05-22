62d958159c21cd1a6db6c6179c1bf20ca6604b21
Zach Aysan <zachaysan@gmail.com>
2014-05-22 15:02:33 -0400

    Create the model

--- spec/factories/access_tokens.rb
+++ spec/factories/access_tokens.rb
@@ -1,8 +1,7 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :access_token do
     token "MyString"
-    user_id 1
+
+    association :user, factory: :user
   end
 end
